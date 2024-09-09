7abce65cefa57b6ee82335ff427d909ebc04a479
Amy Haywood Dutton <me@amyhaywood.com>
2024-09-09 01:41:58 -0600

    ✨ Updated types for Users

--- api/src/graphql/users.sdl.ts
+++ api/src/graphql/users.sdl.ts
@@ -1,8 +1,34 @@
 export const schema = gql`
   type User {
     id: Int!
+    firstName: String!
+    lastName: String!
     email: String!
+    username: String!
     avatar: String
+    cover: String
+    bio: String
+    location: String
+    website: String
+    public: Boolean
+    directMessage: Boolean
+    onboarded: Boolean
+    reported: Boolean
+    likes: [Like]!
+    comments: [Comment]!
+    bookmarks: [Bookmark]!
+    posts: [Post]!
+    plans: [Plan]!
+    messagesTo: [Message]!
+    messagesFrom: [Message]!
+    attendingPlans: [AttendingPlan]!
+    mutedBy: [Muted]!
+    mutedUser: [Muted]!
+    attachments: [Attachment]!
+    attending: [Attending]!
+    organization: [Organization]!
+    blockedByUser: [Block]!
+    blockedUser: [Block]!
     createdAt: DateTime!
     updatedAt: DateTime!
   }
@@ -13,8 +39,19 @@ export const schema = gql`
   }
 
   input UpdateUserInput {
+    firstName: String
+    lastName: String
     email: String
+    username: String
     avatar: File
+    cover: File
+    bio: String
+    location: String
+    website: String
+    public: Boolean
+    directMessage: Boolean
+    onboarded: Boolean
+    reported: Boolean
   }
 
   type Mutation {
